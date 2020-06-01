89b0f6f4fc556b37ccfd66095b09b0494ed9e0f0
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-05-31 23:53:05 -0400

    Updated the seed files

--- database/seeds/03-redux_projects.js
+++ database/seeds/03-redux_projects.js
@@ -1,13 +1,32 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('redux_projects').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('redux_projects').insert([
+        {
+          redux_project_id: 1,
+          redux_project_image: "https://chriscourses.com/img/blog/redux/redux.jpg",
+          redux_project_title: "Redux Project 1",
+          redux_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
+          redux_project_github_link: "https://github.com/tiffany-simionescu",
+          redux_project_live_link: "http://tiffanysimionescu.com/"
+        },
+        {
+          redux_project_id: 2,
+          redux_project_image: "https://chriscourses.com/img/blog/redux/redux.jpg",
+          redux_project_title: "Redux Project 2",
+          redux_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
+          redux_project_github_link: "https://github.com/tiffany-simionescu",
+          redux_project_live_link: "http://tiffanysimionescu.com/"
+        },
+        {
+          redux_project_id: 3,
+          redux_project_image: "https://chriscourses.com/img/blog/redux/redux.jpg",
+          redux_project_title: "Redux Project 3",
+          redux_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
+          redux_project_github_link: "https://github.com/tiffany-simionescu",
+          redux_project_live_link: "http://tiffanysimionescu.com/"
+        }
       ]);
     });
 };

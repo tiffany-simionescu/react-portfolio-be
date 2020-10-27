f6be3a451df04c3c368f192fea11bfdd373b72de
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-10-27 15:57:23 -0400

    Updated SQLite project seed file

--- database/seeds/05-sqlite_projects.js
+++ database/seeds/05-sqlite_projects.js
@@ -7,10 +7,9 @@ exports.seed = function(knex) {
           sqlite_project_id: 1,
           sqlite_project_image: "https://images.pexels.com/photos/3184454/pexels-photo-3184454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           sqlite_project_title: "Authentication API",
-          sqlite_project_description: "",
+          sqlite_project_description: "This project is an API that demonstrates understanding of the following: authentication, Express middleware, JSON Web Tokens, and hashing passwords.",
           sqlite_project_github_link: "https://github.com/tiffany-simionescu/node-auth3-project/tree/tiffany-simionescu",
-          // Create a Heroku Link
-          // sqlite_project_live_link: "http://tiffanysimionescu.com/"
+          sqlite_project_live_link: "https://authentication-api-be.herokuapp.com/"
         }
       ]);
     });

dd0dd2e832ee9753e268dbeb064b0881b97f33d6
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-10-27 00:18:07 -0400

    Updated react and redux seed files

--- database/seeds/02-react_projects.js
+++ database/seeds/02-react_projects.js
@@ -5,27 +5,19 @@ exports.seed = function(knex) {
       return knex('react_projects').insert([
         {
           react_project_id: 1,
-          react_project_image: "https://miro.medium.com/max/1200/0*JWL-dYH8mOxe9EGb.png",
-          react_project_title: "React Project 1",
-          react_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
-          react_project_github_link: "https://github.com/tiffany-simionescu",
-          react_project_live_link: "http://tiffanysimionescu.com/"
+          react_project_image: "https://images.pexels.com/photos/2756406/pexels-photo-2756406.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940 ",
+          react_project_title: "Conway's Game of Life",
+          react_project_description: "The Game of Life is a cellular automaton originally created by John Horton Conway in 1970. This game is played on a 2D grid of cells. Each cell can either be alive or dead. The front end was built using React and Material UI.",
+          react_project_github_link: "https://github.com/tiffany-simionescu/conway-game-of-life",
+          react_project_live_link: "https://tiffany-simionescu.github.io/conway-game-of-life/"
         },
         {
           react_project_id: 2,
-          react_project_image: "https://miro.medium.com/max/1200/0*JWL-dYH8mOxe9EGb.png",
-          react_project_title: "React Project 2",
-          react_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
-          react_project_github_link: "https://github.com/tiffany-simionescu",
-          react_project_live_link: "http://tiffanysimionescu.com/"
-        },
-        {
-          react_project_id: 3,
-          react_project_image: "https://miro.medium.com/max/1200/0*JWL-dYH8mOxe9EGb.png",
-          react_project_title: "React Project 3",
-          react_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
-          react_project_github_link: "https://github.com/tiffany-simionescu",
-          react_project_live_link: "http://tiffanysimionescu.com/"
+          react_project_image: "https://images.pexels.com/photos/209956/pexels-photo-209956.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          react_project_title: "Tackle My Trade",
+          react_project_description: "An NFL Fantasy Football Trade Anaylzer is to help a fan to build their ultimate fantasy league, by making the optinmal trades trades to win their fantasy competitions.  The front end was built using React, Material-UI, LESS, CSS, Styled-Components, Redux, Heroku and  React-Router-Dom.",
+          react_project_github_link: "https://github.com/tiffany-simionescu/nfl-fantasy-fe/tree/master",
+          react_project_live_link: "http://www.tacklemytrade.com/"
         }
       ]);
     });
--- database/seeds/03-redux_projects.js
+++ database/seeds/03-redux_projects.js
@@ -5,27 +5,19 @@ exports.seed = function(knex) {
       return knex('redux_projects').insert([
         {
           redux_project_id: 1,
-          redux_project_image: "https://chriscourses.com/img/blog/redux/redux.jpg",
-          redux_project_title: "Redux Project 1",
-          redux_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
-          redux_project_github_link: "https://github.com/tiffany-simionescu",
-          redux_project_live_link: "http://tiffanysimionescu.com/"
+          redux_project_image: "https://images.pexels.com/photos/247399/pexels-photo-247399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          redux_project_title: "Random Animals App",
+          redux_project_description: "This is an application that provides random pictures for foxes, dogs and cats. The front end was built using React, Redux and React Router DOM.",
+          redux_project_github_link: "https://github.com/tiffany-simionescu/React-Redux-App",
+          redux_project_live_link: "https://tiffany-simionescu.github.io/dog-image"
         },
         {
           redux_project_id: 2,
-          redux_project_image: "https://chriscourses.com/img/blog/redux/redux.jpg",
-          redux_project_title: "Redux Project 2",
-          redux_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
-          redux_project_github_link: "https://github.com/tiffany-simionescu",
-          redux_project_live_link: "http://tiffanysimionescu.com/"
-        },
-        {
-          redux_project_id: 3,
-          redux_project_image: "https://chriscourses.com/img/blog/redux/redux.jpg",
-          redux_project_title: "Redux Project 3",
-          redux_project_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque consectetur libero feugiat consequat. Quisque tempus, nunc vitae scelerisque aliquet, orci est interdum arcu, vitae.",
-          redux_project_github_link: "https://github.com/tiffany-simionescu",
-          redux_project_live_link: "http://tiffanysimionescu.com/"
+          redux_project_image: "https://images.pexels.com/photos/209956/pexels-photo-209956.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          redux_project_title: "Tackle My Trade",
+          redux_project_description: "An NFL Fantasy Football Trade Anaylzer is to help a fan to build their ultimate fantasy league, by making the optinmal trades trades to win their fantasy competitions.  The front end was built using React, Material-UI, LESS, CSS, Styled-Components, Redux, Heroku and  React-Router-Dom.",
+          redux_project_github_link: "https://github.com/tiffany-simionescu/nfl-fantasy-fe/tree/master",
+          redux_project_live_link: "http://www.tacklemytrade.com/"
         }
       ]);
     });

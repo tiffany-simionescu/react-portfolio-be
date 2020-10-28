bd6c864c35caa1d8a8af4c8b267e776f9b218a1d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-10-28 15:11:37 -0400

    updated python seed

--- database/seeds/07-python_projects.js
+++ database/seeds/07-python_projects.js
@@ -8,21 +8,24 @@ exports.seed = function(knex) {
           python_project_image: "https://images.pexels.com/photos/39018/cards-jass-cards-card-game-strategy-39018.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           python_project_title: "Python Tarot Game",
           python_project_description: "This text-based Tarot game is a Python object oriented program that is very simple to use. You can play this game in your CLI. Just answer a few questions and you'll receive your answer.",
-          python_project_github_link: "https://github.com/tiffany-simionescu/python-tarot"
+          python_project_github_link: "https://github.com/tiffany-simionescu/python-tarot",
+          python_project_live_link: "https://github.com/tiffany-simionescu/python-tarot"
         },
         {
           python_project_id: 2,
           python_project_image: "https://images.pexels.com/photos/4614107/pexels-photo-4614107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           python_project_title: "Sorting Algorithms",
           python_project_description: "This project demonstrates different ways to sort data. There are four sorting / searching algorithms covered: iterative sorting, recursive sorting, binary search and linear search.",
-          python_project_github_link: "https://github.com/tiffany-simionescu/Sorting/tree/tiffany-simionescu"
+          python_project_github_link: "https://github.com/tiffany-simionescu/Sorting/tree/tiffany-simionescu",
+          python_project_live_link: "https://github.com/tiffany-simionescu/Sorting/tree/tiffany-simionescu"
         },
         {
           python_project_id: 3,
           python_project_image: "https://images.pexels.com/photos/9198/nature-sky-twilight-grass-9198.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           python_project_title: "Graphs",
           python_project_description: "This project demonstrates the use of graphs. The objectives achieved were graph representation, breadth first search, depth first search, randomness and connected components.",
-          python_project_github_link: "https://github.com/tiffany-simionescu/Graphs"
+          python_project_github_link: "https://github.com/tiffany-simionescu/Graphs",
+          python_project_live_link: "https://github.com/tiffany-simionescu/Graphs"
         }
       ]);
     });

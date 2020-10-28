8a44744d3ba0d3d637d707b737c495006a316cd8
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-10-27 22:36:50 -0400

    Testing Description

--- database/seeds/06-postgresql_projects.js
+++ database/seeds/06-postgresql_projects.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
           postgresql_project_id: 1,
           postgresql_project_image: "https://images.pexels.com/photos/209956/pexels-photo-209956.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           postgresql_project_title: "Tackle My Trade",
-          postgresql_project_description: "An NFL Fantasy Football Trade Anaylzer is to help a fan to build their ultimate fantasy league, by making the optinmal trades to win their fantasy competitions. The back end was built using Express, NodeJS, PostgreSQL, SQLite3 and Knex.",
+          postgresql_project_description: "",
           postgresql_project_github_link: "https://github.com/tiffany-simionescu/nfl-fantasy-be",
           postgresql_project_live_link: "https://data.heroku.com/dataclips/rwhkccnmdgnbgemujsjivyshywlb"
         },
@@ -15,7 +15,7 @@ exports.seed = function(knex) {
           postgresql_project_id: 2,
           postgresql_project_image: "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           postgresql_project_title: "Portfolio API",
-          postgresql_project_description: "This API is the current back end for this portfolio site. The back end was built using Express, NodeJS, PostgreSQL, SQLite3 and Knex.",
+          postgresql_project_description: "",
           postgresql_project_github_link: "https://github.com/tiffany-simionescu/react-portfolio-be",
           postgresql_project_live_link: "https://tiffany-simionescu-portfolio.herokuapp.com/"
         }
